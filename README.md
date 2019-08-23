4ac4789772fcd0d88e1da7640c034cb410ab2161
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-08-23 22:26:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cookie Block
+A cookie block. https://curseforge.com/minecraft/mc-mods/cookie-block
---
+++ src/main/java/bl4ckscor3/mod/cookieblock/CookieBlock.java
@@ -0,0 +1,52 @@
+package bl4ckscor3.mod.cookieblock;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Food;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReader;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.registries.ObjectHolder;
+
+@Mod(CookieBlock.MODID)
+@EventBusSubscriber(bus=Bus.MOD)
+public class CookieBlock
+{
+	public static final String MODID = "cookieblock";
+	@ObjectHolder(MODID + ":" + MODID)
+	public static final Block COOKIE_BLOCK = null;
+
+	@SubscribeEvent
+	public static void registerBlock(RegistryEvent.Register<Block> event)
+	{
+		event.getRegistry().register(new Block(Block.Properties.create(Material.EARTH).hardnessAndResistance(0.25F)) {
+			@Override
+			public SoundType getSoundType(BlockState state)
+			{
+				return SoundType.STONE;
+			}
+
+			@Override
+			public SoundType getSoundType(BlockState state, IWorldReader world, BlockPos pos, Entity entity)
+			{
+				return SoundType.STONE;
+			}
+		}.setRegistryName(MODID, MODID));
+	}
+
+	@SubscribeEvent
+	public static void registerItem(RegistryEvent.Register<Item> event)
+	{
+		event.getRegistry().register(new BlockItem(COOKIE_BLOCK, new Item.Properties().group(ItemGroup.FOOD).food(new Food.Builder().hunger(18).saturation(0.9F).build())).setRegistryName(COOKIE_BLOCK.getRegistryName()));
+	}
+}
