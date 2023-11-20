2b475f6d425e5105686185f91ad7914077a24cc6
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-11-20 14:23:30 +0100

    fix compatibility with NeoForge 20.2.59-beta

--- src/main/java/bl4ckscor3/mod/cookieblock/CookieBlock.java
+++ src/main/java/bl4ckscor3/mod/cookieblock/CookieBlock.java
@@ -9,24 +9,25 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.common.Mod.EventBusSubscriber;
 import net.neoforged.fml.common.Mod.EventBusSubscriber.Bus;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import net.neoforged.neoforge.registries.ForgeRegistries;
-import net.neoforged.neoforge.registries.RegistryObject;
 
 @Mod(CookieBlock.MODID)
 @EventBusSubscriber(bus = Bus.MOD)
 public class CookieBlock {
 	public static final String MODID = "cookieblock";
-	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-	public static final RegistryObject<Block> COOKIE_BLOCK = BLOCKS.register(MODID, () -> new Block(Block.Properties.of().strength(0.25F).sound(SoundType.STONE)));
-	public static final RegistryObject<BlockItem> COOKIE_BLOCK_ITEM = ITEMS.register(MODID, () -> new BlockItem(COOKIE_BLOCK.get(), new Item.Properties().food(new FoodProperties.Builder().nutrition(18).saturationMod(0.9F).build())));
+	public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
+	public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+	public static final DeferredBlock<Block> COOKIE_BLOCK = BLOCKS.registerBlock(MODID, BlockBehaviour.Properties.of().strength(0.25F).sound(SoundType.STONE));
+	public static final DeferredItem<BlockItem> COOKIE_BLOCK_ITEM = ITEMS.registerBlockItem(COOKIE_BLOCK, new Item.Properties().food(new FoodProperties.Builder().nutrition(18).saturationMod(0.9F).build()));
 
 	public CookieBlock(IEventBus modEventBus) {
 		BLOCKS.register(modEventBus);
